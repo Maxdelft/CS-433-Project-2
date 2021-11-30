5b365c21b0b8f1743eaf9f611a78547e403c523f
Maximilian van Amerongen <max.van99@t-online.de>
2021-11-30 12:36:07 +0100

    comment added to file for better readability

--- Python_Scripts/Python_Scripts_Max/gen_feat_matrix.py
+++ Python_Scripts/Python_Scripts_Max/gen_feat_matrix.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Nov 28 21:57:33 2021
-
-@author: maximilianvanamerongen
+This file takes as an input the excel files where the answers are saved if discomfort is experienced.
+Those answers are stored as yes/no and they are translated to 1/0.
+The translation is stored in Y_2A_bin.
 """
 
 from sklearn.feature_selection import SelectKBest
@@ -11,26 +12,27 @@ import pandas as pd
 import os,sys
 import numpy as np
 
-feature_path = '/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A'
-X_2A = np.load("/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A/Arrays/X_2A.npy")
-Y_2A = np.load("/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A/Arrays/Y_2A.npy")
-results = pd.read_excel (r'/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Excel_Sheets/results_v211118_CS433.xlsx')
-
-#%%
-
+feature_path    = '/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A'
+X_2A            = np.load("/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A/Arrays/X_2A.npy")
+image_names_2A  = np.load("/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Data/Expt_2A/Arrays/image_names_2A.npy")
+results         = pd.read_excel (r'/Users/maximilianvanamerongen/Documents/Master/EPFL/Machine_Learning_CS433/Project2/CS-433-Project-2-local/Excel_Sheets/results_v211118_CS433.xlsx')
+#%% Extract needed data from excel table:
 answers     = pd.DataFrame(results, columns= ['Are you experiencing any discomfort due to glare at the moment?']).to_numpy()
 store_index = pd.DataFrame(results, columns = ['index']).to_numpy()
 part_index  = pd.DataFrame(results, columns = ['Participant Index'])
 
+# Translate answer to binary answer:
 answer_bin = translate_answers(answers)
-Y_2A_bin   = create_bin_Y(Y_2A,answer_bin,store_index)
-
-    
-#%%  
-def create_bin_Y(Y_label,answer_bin,store_index):
+Y_2A_bin   = create_bin_Y(image_names_2A,answer_bin,store_index)
+#%%  This function
+def create_bin_Y(image_names,answer_bin,store_index):
+    # input : image_names : image_names belonging to considered experiment 
+    #         answer_bin  : binarary answers of all answers listed in excel
+    #         store_index : image names of all images listed in excel
+    # output: Y_bin       : binary answers of images considered in the experiment
     Y_bin = np.array([])
-    for j in range(len(Y_label)):
-        Y_string = Y_label[j]
+    for j in range(len(image_names)):
+        Y_string = image_names[j]
         count = 0
         for i in store_index:
             if Y_string[:-4] in str(i):
@@ -38,9 +40,10 @@ def create_bin_Y(Y_label,answer_bin,store_index):
                 break
             count += 1
     return Y_bin        
-#%%
-
+#%% This function translates a 'Yes' to 1 and a 'No' to 0
 def translate_answers(answers):
+    # input: answers in Yes/No format 
+    # output: answers in 1/0 format
     answer_bin = np.array([]);
     for answer in answers:
         if answer == 'Yes':
